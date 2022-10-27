073bf48b312ed7d4ad4ab49efe23f50c42294cee
gracefarkh <aizhamalfarkhatovna@mail.ru>
2022-10-27 14:28:45 +0300

    dz6

---
+++ app.js
@@ -0,0 +1,12 @@
+const obj2 = {
+    height:200,
+    width:300,
+    title:"My block"
+}
+console.log(obj2);
+
+function func(){
+    console.log(obj2.height**2);
+    console.log(obj2.width**2)
+}
+func();
